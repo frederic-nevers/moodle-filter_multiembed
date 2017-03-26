ce4a7af76fdc8fa0e4a04e5aa561befc954de436
NEVERS, Frederic Jean-Pierre <fnevers@isf.edu.hk>
2017-03-26 23:19:06 +0800

    Version 3.2.1

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version   = 2017032701;
 $plugin->requires  = 2015051108;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '3.2';
+$plugin->release = '3.2.1';
 $plugin->component = 'filter_multiembed';
