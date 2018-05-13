6fa3e19509c2889acd98bbc9ab71b6520de9a66c
NEVERS, Frederic Jean-Pierre <fnevers@isf.edu.hk>
2018-05-13 19:27:21 +0800

    Tested compatibility with Moodle 3.5 - No changes

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2017110401;
+$plugin->version   = 2018051301;
 $plugin->requires  = 2015051108;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '3.4';
+$plugin->release = '3.5';
 $plugin->component = 'filter_multiembed';
