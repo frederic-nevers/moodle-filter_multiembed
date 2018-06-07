596a7b0dcbc67bb0c37e942b1ffeb90eadc139a9
basilstotz <basil.stotz@gmail.com>
2018-06-07 20:15:23 +0200

    add learningapps.org

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2018052201;
+$plugin->version   = 2018060601;
 $plugin->requires  = 2015051108;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release = '3.5.2';
